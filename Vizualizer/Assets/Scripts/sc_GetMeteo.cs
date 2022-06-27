2ae7c43875b328eda334ac19bc1929399830eb22
Maltiranis <valentin.paillot@orange.fr>
2022-06-27 17:58:19 +0200

    Sauvegarde html dans un .txt
    Manque la recherche par mot clé (voir lien discord)

--- Vizualizer/Assets/Scripts/sc_GetMeteo.cs
+++ Vizualizer/Assets/Scripts/sc_GetMeteo.cs
@@ -1,17 +1,29 @@
+using System.Collections.Generic;
 using System.Collections;
 using UnityEngine;
 using TMPro;
 using UnityEngine.Networking;
+using System.IO;
+using System.Linq;
 
 public class sc_GetMeteo : MonoBehaviour
 {
     [TextArea (10, 10)]
     public string textFromWWW;
-    private string url = "https://weather.com/fr-FR/temps/aujour/l/7d6d3f6ca4633a40596d4624c65f780c32635fbf8233041ec8be134f9bee529b"; // <-- enter your url here
+    private string url = "https://weather.com/fr-FR/temps/aujour/l/7d6d3f6ca4633a40596d4624c65f780c32635fbf8233041ec8be134f9bee529b"; //url here
+    //private string url = "https://www.google.com/search?q=meteo&rlz=1C1CHBF_frFR925FR947&biw=2133&bih=1041&sxsrf=ALiCzsa_YutVzxyzs_I7H-c0nYgq9_OMfg%3A1656329034910&ei=SpO5YtmKN-KAzgO5zYFA&ved=0ahUKEwjZ14a-ws34AhVigHMKHblmAAgQ4dUDCA4&uact=5&oq=meteo&gs_lcp=Cgdnd3Mtd2l6EAMyBAgAEEMyBwgAEMkDEEMyBQgAEJIDMgsIABCABBCxAxCDATIECAAQQzIECAAQAzIECAAQQzIRCC4QgAQQsQMQgwEQxwEQ0QMyBAgAEEMyBAgAEEM6BwgAEEcQsAM6BwgAELADEEM6CggAEOQCELADGAE6CggAELEDEIMBEEM6EAguELEDEIMBEMcBENEDEEM6DAgAEMkDEEMQRhCAAkoECEEYAEoECEYYAVDhCFiUEGClEmgDcAF4AIAB3AKIAa8GkgEHMi4xLjEuMZgBAKABAcgBEcABAdoBBggBEAEYCQ&sclient=gws-wiz"; //url here
+
+    public string temperature;
 
     void Start()
     {
         StartCoroutine(GetTextFromWWW());
+    }
+
+    void SetVariables ()
+    {
+        if (textFromWWW.Contains)
+            temperature = textFromWWW.
     }
 
     IEnumerator GetTextFromWWW()
@@ -27,6 +39,11 @@ public class sc_GetMeteo : MonoBehaviour
         else
         {
             textFromWWW = www.text;
+            StreamWriter writer = new StreamWriter("Assets/Resources/MeteoParser.txt", true);
+            writer.Flush();
+            writer.Write(textFromWWW);
+            
+            writer.Close();
         }
     }
 }
